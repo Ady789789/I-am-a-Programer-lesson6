12971c10aece06bbebff86f23358d76ada332e87
adina778899 <adinamemetea@yahoo.com>
2025-01-23 17:46:55 -0600

    Initial Commit

---
+++ I am a Programer!/I_am_a_Programer_App.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_a_Programer_App.swift
+//  I am a Programer!
+//
+//  Created by natural health on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct I_am_a_Programer_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
